c097c518ba1d65309ea7129a3bb0b880684201b9
adinathdhavale <87268133+adinathdhavale@users.noreply.github.com>
2021-07-14 19:49:35 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,37 @@
+package lnfyTest;
+
+public class Department implements Cloneable{
+
+	
+	private int id;
+    private String name;
+ 
+    public Department(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	protected Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+		
+	}
+    
+}
